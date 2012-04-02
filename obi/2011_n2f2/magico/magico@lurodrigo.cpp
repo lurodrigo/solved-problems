e63c30e271d1391cee15c89d0beef8eab0ff1fb9
Luiz Souza <contact@lurodrigo.com>
2012-04-02 19:13:02 -0400

    Completada a resolução da questão "quadrado mágico"

--- obi/2011_n2f2/magico/magico@lurodrigo.cpp
+++ obi/2011_n2f2/magico/magico@lurodrigo.cpp
@@ -13,7 +13,43 @@
 #include <iostream>
 using namespace std;
 
+const int MAX = 1001;
+int diag;
+int col[MAX], row[MAX];
+bool usado[1000001];
+
 int main() {
+
+    int n, i, j, x;
+    bool eMagico = true;
+    
+    cin >> n;
+    
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            cin >> x;
+            
+            if (x < 1 || x > 1000000 || usado[x]) {
+                eMagico = false;
+            } else {
+                usado[x] = true;
+                
+                row[i] += x;
+                col[j] += x;
+                
+                if ( i == j )
+                    diag += x;
+            }
+        }
+    }
+    
+    for (i = 0; i < n; i++)
+        eMagico &= (col[i] == diag) && (row[i] == diag);
+        
+    if ( eMagico )
+        cout << diag;
+    else
+        cout << 0;
 
     return 0;
 }
