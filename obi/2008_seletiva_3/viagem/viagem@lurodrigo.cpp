196d074c34db9f2d9c8483784d71f5ad91af166a
Luiz Rodrigo <luizrodri.go@hotmail.com>
2012-05-25 09:03:34 -0400

    Progresso

--- obi/2008_seletiva_3/viagem/viagem@lurodrigo.cpp
+++ obi/2008_seletiva_3/viagem/viagem@lurodrigo.cpp
@@ -8,6 +8,21 @@
     Autor da solução: Luiz Rodrigo <@lurodrigo> <luizrodri.go@hotmail.com>
     Tags: programacao-dinamica
     Complexidade: O(n²t)
+    
+    m(i, k) = máximo de cidades que se pode viajar com tarifa máxima k
+    terminando na cidade i
+    
+    m(i, k) = max { 1 + m[j][k-cost(j, i)] | j < i }
+    sendo cost(x, y) = |x.lat - y.lat|
+    
+    Isso funciona normalmente para cidades ordenadas pela longitude, sendo
+    todas as longitudes distintas. Entretanto, as coisas ficam mais complicadas
+    quando as longitudes não são todas distintas, pois podemos começar do sul
+    e ir para o norte, do norte para o sul, ou ficar subindo e descendo.
+    
+    Na verdade, é fácil provar que só os dois primeiros casos são
+    necessários de se considerar. Abaixo são computadas as melhores escolhas
+    considerando ambos os casos, ficando somente com a melhor das duas.
  
 */
 
@@ -16,12 +31,14 @@
 #include <algorithm>
 using namespace std;
 
+const int MAX_N = 501;
+
 struct City {
     int lat, lon;
-} cities[501];
+} cities[MAX_N];
 
-int m[501][501];
-int m1[501], m2[501], next[501];
+int m[MAX_N][MAX_N], m_asc[MAX_N][MAX_N], m_desc[MAX_N][MAX_N];
+int next[MAX_N];
 
 bool comp(City a, City b) {
     if ( a.lon != b.lon )
@@ -30,14 +47,22 @@ bool comp(City a, City b) {
         return a.lat < b.lat;
 }
 
-void compute_step(int x, int k, int r[]) {
+void compute_step(int x, int k, int mx[MAX_N][MAX_N]) {
     int i, j, diff;
     
-    for (i = x; i < next[x]; i++)
-        for (r[i] = m[i][max(0, k-1)], j = 0; j < i; j++) {
+    for (i = x; i < next[x]; i++) {
+        mx[i][k] = m[i][max(0, k-1)];
+        
+        for (j = 0; j < x; j++) {
             diff = abs(cities[i].lat - cities[j].lat);
-            r[i] = max(m1[i], (diff <= k) ? 1 + m[j][k-diff] : 0);
+            mx[i][k] = max(mx[i][k], (diff <= k) ? 1 + m[j][k-diff] : 0);
         }
+        
+        for (j = x; j < i; j++) {
+            diff = abs(cities[i].lat - cities[j].lat);
+            mx[i][k] = max(mx[i][k], (diff <= k) ? 1 + mx[j][k-diff] : 0);
+        }
+    }
 }
 
 int main() {
@@ -61,42 +86,18 @@ int main() {
     
     for (k = 0; k <= t; k++) { 
         for (i = 1; i <= n; i = next[i]) {
-        
-            /*
-                m(i, k) = máximo de cidades que se pode viajar com tarifa máxima k
-                terminando na cidade i
-                
-                m(i, k) = max { 1 + m[j][k-cost(j, i)] | j < i }
-                sendo cost(x, y) = |x.lat - y.lat|
-                
-                Isso funciona normalmente para cidades ordenadas pela longitude, sendo
-                todas as longitudes distintas. Entretanto, as coisas ficam mais complicadas
-                quando as longitudes não são todas distintas, pois podemos começar do sul
-                e ir para o norte, do norte para o sul, ou ficar subindo e descendo.
-                
-                Na verdade, é fácil provar que só os dois primeiros casos são
-                necessários de se considerar. Abaixo são computadas as melhores escolhas
-                considerando ambos os casos, ficando somente com a melhor das duas.
-            */
-        
-            compute_step(i, k, m1); 
+            compute_step(i, k, m_asc); 
             reverse(cities + i, cities + next[i]);
-            compute_step(i, k, m2);
+            compute_step(i, k, m_desc);
             reverse(cities + i, cities + next[i]);
-            reverse(m2 + i, m2 + next[i]);
             
-            for (j = i; j < next[i]; j++) {
-                m[j][k] = max(m1[j], m2[j]);
-                cout << m[j][k] << " ";
-            }
+            for (j = 0; j < next[i]-i; j++) 
+                m[i+j][k] = max(m_asc[i+j][k], m_desc[next[i]-j-1][k]);
         }
-       
-        cout << "\n";
+        
     }
+   
+    cout << m[n][t]-1 << endl;
     
-    
-    
-    cout << m[n][t]-1;
-
     return 0;
 }
