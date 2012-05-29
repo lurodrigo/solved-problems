25d0b77b14629694f8401597170fd1a63f5fe3d8
Luiz Souza <contact@lurodrigo.com>
2012-05-29 00:22:38 -0400

    Criado template para os próximos problems

---
+++ obi/2003_seletiva/festa/festa@lurodrigo.cpp
@@ -0,0 +1,21 @@
+
+/*
+    Olimpíada Brasileira de Informática
+    Seletiva para a IOI, 2003
+    Problema: Festa Junina
+    
+    Data de submissão: 28/05/2012
+    Autor da solução: Luiz Rodrigo <@lurodrigo> <luizrodri.go@hotmail.com>
+    Tags: backtracking, np-hard
+    Complexidade: O(n²*2^n)  
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+
+    return 0;
+}
